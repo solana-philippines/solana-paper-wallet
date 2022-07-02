4f28834d12e28d0feed86de3fe4014a40f0c9ba1
Kristian Quirapas <kmquirapas@gmail.com>
2022-07-03 01:33:33 +0800

    Done with ugly draft contract, time for client test

--- tests/integration.rs
+++ tests/integration.rs
@@ -25,52 +25,52 @@ pub struct RedeemPayload {
     hash: Pubkey
 }
 
-#[test]
-fn test_store_sol() {
-    solana_logger::setup_with_default("solana_runtime::message=debug");
-
-    let program_id = Pubkey::new_unique();
-
-    let (test_validator, payer) = TestValidatorGenesis::default()
-        .add_program("target/deploy/solana_paper_wallet", program_id)
-        .start();
-
-    let rpc_client = test_validator.get_rpc_client();
-
-    let blockhash = rpc_client.get_latest_blockhash().unwrap();
-
-    // Construct instruction_data
-    let payload_struct = StorePayload {
-        code: "mycode".to_string()
-    };
-
-    let mut payload = payload_struct.try_to_vec().unwrap();
-    let mut instruction_data = vec![0];
-
-    instruction_data.append(&mut payload);
-
-    let (pda, bump_seed) = Pubkey::find_program_address(&[payload_struct.code.as_bytes(), payer.pubkey().as_ref()], &program_id);
-
-    let account_data = vec![
-      AccountMeta::new(payer.pubkey(), true),
-      AccountMeta::new(pda, false),
-      AccountMeta::new(system_program::ID, false),
-    ];
-
-    let mut transaction = Transaction::new_with_payer(
-        &[
-          Instruction {
-              program_id,
-              accounts: account_data,
-              data: instruction_data,
-          },
-        ],
-        Some(&payer.pubkey())
-    );
-    transaction.sign(&[&payer], blockhash);
-
-    assert_matches!(rpc_client.send_and_confirm_transaction(&transaction), Ok(_));
-}
+// #[test]
+// fn test_store_sol() {
+//     solana_logger::setup_with_default("solana_runtime::message=debug");
+//
+//     let program_id = Pubkey::new_unique();
+//
+//     let (test_validator, payer) = TestValidatorGenesis::default()
+//         .add_program("target/deploy/solana_paper_wallet", program_id)
+//         .start();
+//
+//     let rpc_client = test_validator.get_rpc_client();
+//
+//     let blockhash = rpc_client.get_latest_blockhash().unwrap();
+//
+//     // Construct instruction_data
+//     let payload_struct = StorePayload {
+//         code: "mycode".to_string()
+//     };
+//
+//     let mut payload = payload_struct.try_to_vec().unwrap();
+//     let mut instruction_data = vec![0];
+//
+//     instruction_data.append(&mut payload);
+//
+//     let (pda, bump_seed) = Pubkey::find_program_address(&[payload_struct.code.as_bytes(), payer.pubkey().as_ref()], &program_id);
+//
+//     let account_data = vec![
+//       AccountMeta::new(payer.pubkey(), true),
+//       AccountMeta::new(pda, false),
+//       AccountMeta::new(system_program::ID, false),
+//     ];
+//
+//     let mut transaction = Transaction::new_with_payer(
+//         &[
+//           Instruction {
+//               program_id,
+//               accounts: account_data,
+//               data: instruction_data,
+//           },
+//         ],
+//         Some(&payer.pubkey())
+//     );
+//     transaction.sign(&[&payer], blockhash);
+//
+//     assert_matches!(rpc_client.send_and_confirm_transaction(&transaction), Ok(_));
+// }
 
 #[test]
 fn test_redeem_sol() {
@@ -149,9 +149,6 @@ fn test_redeem_sol() {
 
     let (pda, bump_seed) = Pubkey::find_program_address(&[payload_struct.code.as_bytes(), payer.pubkey().as_ref()], &program_id);
 
-    let balance = rpc_client.get_balance(&user2.pubkey()).expect("Failed to get balance from rpc_client");
-    println!("before user2 balance: {:?}", balance);
-
     let account_data = vec![
       AccountMeta::new(user2.pubkey(), true),
       AccountMeta::new(pda, false),
@@ -170,10 +167,12 @@ fn test_redeem_sol() {
         Some(&user2.pubkey())
     );
     transaction.sign(&[&user2], blockhash);
-
-    let balance = rpc_client.get_balance(&user2.pubkey()).expect("Failed to get balance from rpc_client");
-    println!("after user2 balance: {:?}", balance);
+    rpc_client.send_and_confirm_transaction(&transaction).expect("hotdog");
 
 
-    assert_matches!(rpc_client.send_and_confirm_transaction(&transaction), Ok(_));
+    let new_balance = rpc_client.get_balance(&user2.pubkey()).expect("Failed to get balance from rpc_client");
+    println!("after user2 balance: {:?}", new_balance);
+    println!("user2: {:?}", new_balance);
+
+    // assert_matches!(rpc_client.send_and_confirm_transaction(&transaction), Ok(_));
 }
